c2925a1e1b18608e7a50e8d20c446bc1d5d0e39c
Sabbir Hossain Rizvi <rizvibd1100@gmail.com>
2026-05-03 02:02:17 +0600

    Fix baseURL in auth-client to include trailing slash

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "https://suncart-pink.vercel.app",
+  baseURL: "https://suncart-pink.vercel.app/",
   // baseURL: "http://localhost:3000",
 });
 
